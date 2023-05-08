7cafb0c75541b5a4923513fa32b121e0321d0811
ingusrv <ingus.valheims@gmail.com>
2023-05-08 19:16:07 +0300

    datu salīdzināšana pēc FPS

--- web/pages/data/data.js
+++ web/pages/data/data.js
@@ -1,3 +1,6 @@
+let dataToCompare = [];
+let checkboxes = undefined;
+
 fetch("/api/v1/driveData").then(async (res) => {
     const table = document.querySelector("#run-table");
     const body = await res.json();
@@ -21,4 +24,73 @@ fetch("/api/v1/driveData").then(async (res) => {
         `;
         table.appendChild(tr);
     });
+
+    checkboxes = document.querySelectorAll("[data-select-data-checkbox]");
+    loadCheckboxes(checkboxes);
+});
+
+function loadCheckboxes(checkboxes) {
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener("click", (e) => {
+            const label = e.target.dataset.label;
+            const fps = e.target.dataset.fps;
+            const data = { label: label, fps: fps };
+            console.log(data);
+
+            for (item of dataToCompare) {
+                if (item.label == data.label && item.fps == data.fps) {
+                    console.log("dati sakrīt");
+                    const index = dataToCompare.indexOf(item);
+                    console.log("index", index);
+                    dataToCompare.splice(index, 1);
+                    console.log("data", dataToCompare);
+                    return;
+                }
+            }
+            console.log("dati nesakrīt");
+            dataToCompare.push(data);
+            console.log("data", dataToCompare);
+        });
+    });
+}
+
+const compareButton = document.querySelector("#compare-data");
+const dialog = document.querySelector("#chart-dialog");
+const ctx = document.getElementById('compare-chart');
+
+const closeDialog = document.querySelector("#close-dialog");
+
+compareButton.addEventListener("click", (e) => {
+    let labels = []
+    let data = []
+    for (item of dataToCompare) {
+        labels.push(item.label);
+        data.push(item.fps);
+    }
+
+    dialog.showModal();
+
+    let chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Vidējais FPS',
+                data: data,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    closeDialog.addEventListener("submit", (e) => {
+        console.log("dialog closed");
+        chart.destroy();
+    })
 });
