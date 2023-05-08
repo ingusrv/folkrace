0635adcdef43c487271053497d6b47a4715472c0
ingusrv <ingus.valheims@gmail.com>
2023-05-08 19:07:00 +0300

    papildus dati katram braucienam

--- web/pages/data/data.js
+++ web/pages/data/data.js
@@ -7,7 +7,18 @@ fetch("/api/v1/driveData").then(async (res) => {
         const tr = document.createElement("tr");
         // TODO: fix potential XSS
         // TODO: replace table with message if no data or error
-        tr.innerHTML = `<td>${++i}</td><td>${item.createdAt}</td><td>${item.elapsedTime}</td><td>${item.data}</td>`;
+        tr.innerHTML = `
+        <td><input class="select-data-checkbox" type="checkbox" name="select-data-checkbox" data-select-data-checkbox data-label="${item.label}" data-elapsed-time="${item.elapsedTime}" data-fps="${item.fps}"></td>
+        <td>${++i}</td>
+        <td>${item.robotId}</td>
+        <td>${item.createdAt}</td>
+        <td>${item.algorithm}</td>
+        <td>${item.version}</td>
+        <td>${item.label}</td>
+        <td>${item.fps}</td>
+        <td>${item.elapsedTime}</td>
+        <td class="short-data">${item.data}</td>
+        `;
         table.appendChild(tr);
     });
 });
