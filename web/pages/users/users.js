6449c4ae7a3122efc94baf164960d00cf64e3f90
ingusrv <ingus.valheims@gmail.com>
2023-06-05 12:40:00 +0300

    jauns lietotāju UI un kods

--- web/pages/users/users.js
+++ web/pages/users/users.js
@@ -1,139 +1,113 @@
 import Notification from "../notification.js";
 
 const addUser = document.querySelector("#add-user");
-const cancelAddUser = document.querySelector("#cancel-add-user");
-const addUserForm = document.querySelector("#add-user-form");
-const removeUser = document.querySelector("#remove-user");
-const cancelRemoveUser = document.querySelector("#cancel-remove-user");
-const removeUserForm = document.querySelector("#confirm-remove-user");
-let usersToDelete = [];
-let checkboxes = undefined;
+const addUserDialog = document.querySelector("#add-user-dialog");
+const userTable = document.querySelector("#user-table").querySelector("tbody");
 
-fetch("/api/v1/users").then(async (res) => {
-    const table = document.querySelector("#user-table");
-    const body = await res.json();
-    console.log(body);
+function loadUsers() {
+    userTable.replaceChildren();
+    fetch("/api/v1/users").then(async (res) => {
+        if (res.status >= 400) {
+            new Notification({ type: "error", text: "Notika kļūda ielādējot lietotāju datus!" });
+            return;
+        }
 
-    // TODO: fix potential XSS
-    // TODO: replace table with message if no data or error
-    body.data.forEach((item, i) => {
-        const tr = document.createElement("tr");
-        tr.innerHTML = `<td><input class="remove-user-checkbox" type="checkbox" name="remove-user-checkbox" data-remove-user-checkbox data-username="${item.username}"></td><td>${++i}</td><td>${item.username}</td><td>${item.admin}</td><td>${item.createdAt}</td>`;
-        table.appendChild(tr);
-    });
-
-    checkboxes = document.querySelectorAll("[data-remove-user-checkbox]");
-    loadCheckboxes(checkboxes);
-});
-
-addUser.addEventListener("click", () => {
-    document.querySelector("#dim-background").style.opacity = 0.8;
-    document.querySelector("#dim-background").classList.toggle("hidden");
-    document.querySelector("#add-user-prompt").classList.toggle("hidden");
-});
-
-cancelAddUser.addEventListener("click", () => {
-    document.querySelector("#dim-background").style.opacity = 0.8;
-    document.querySelector("#dim-background").classList.toggle("hidden");
-    document.querySelector("#add-user-prompt").classList.toggle("hidden");
-});
-
-// TODO: make this more responsive
-addUserForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const user = {
-        username: document.querySelector("#username").value,
-        password: document.querySelector("#password").value,
-        admin: document.querySelector("#isAdmin").checked
-    };
-
-    fetch("/api/v1/user", {
-        method: "POST",
-        mode: "same-origin",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(user)
-    }).then(async (res) => {
         const body = await res.json();
         console.log(body);
-        if (res.status >= 400) {
-            new Notification({ type: "error", text: body.message });
-        }
-        if (res.status === 201) {
-            new Notification({ type: "success", text: body.message });
-        }
 
-        document.querySelector("#dim-background").style.opacity = 0.8;
-        document.querySelector("#dim-background").classList.toggle("hidden");
-        document.querySelector("#add-user-prompt").classList.toggle("hidden");
-    });
-});
+        body.data.forEach((user, i) => {
+            const row = document.createElement("tr");
 
-removeUser.addEventListener("click", () => {
-    document.querySelector("#dim-background").style.opacity = 0.8;
-    document.querySelector("#dim-background").classList.toggle("hidden");
-    document.querySelector("#remove-user-prompt").classList.toggle("hidden");
-});
+            // dati
+            const number = document.createElement("td");
+            number.textContent = ++i;
+            const username = document.createElement("td");
+            username.textContent = user.username;
+            const isAdmin = document.createElement("td");
+            isAdmin.textContent = user.admin;
+            const createdAt = document.createElement("td");
+            createdAt.textContent = user.createdAt;
 
-cancelRemoveUser.addEventListener("click", () => {
-    document.querySelector("#dim-background").style.opacity = 0.8;
-    document.querySelector("#dim-background").classList.toggle("hidden");
-    document.querySelector("#remove-user-prompt").classList.toggle("hidden");
-});
+            // darbības
+            const actions = document.createElement("td");
+            const editButton = document.createElement("button");
+            editButton.textContent = "Rediģēt";
+            editButton.classList.add("button", "secondary");
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Noņemt";
+            deleteButton.classList.add("button", "danger");
 
-function loadCheckboxes(checkboxes) {
-    checkboxes.forEach((checkbox) => {
-        checkbox.addEventListener("click", (e) => {
-            const username = e.target.dataset.username;
-            console.log(username);
-            if (!usersToDelete.includes(username)) {
-                usersToDelete.push(username);
-                return;
-            }
+            actions.append(editButton, deleteButton);
 
-            const index = usersToDelete.indexOf(username);
-            usersToDelete.splice(index, 1);
+            // pogu funkcionalitāte
+            editButton.addEventListener("click", () => { });
+            deleteButton.addEventListener("click", () => {
+                fetch(`/api/v1/user/${user.username}`, {
+                    method: "DELETE",
+                    mode: "same-origin",
+                }).then(async (res) => {
+                    const data = await res.json();
+                    console.log(data);
+
+                    if (res.status >= 400) {
+                        new Notification({ type: "error", text: data.message });
+                        return;
+                    }
+
+                    new Notification({ type: "success", text: data.message });
+                    // nav optimāli pārlādēt visu sarakstu, bet pagaidām strādā
+                    loadUsers();
+                });
+            });
+
+            row.append(number, username, isAdmin, createdAt, actions);
+            userTable.appendChild(row);
         });
     });
 }
 
-removeUserForm.addEventListener("click", () => {
-    console.log(usersToDelete);
-    let removedCount = 0;
-    let unableToRemoveCount = 0;
-    usersToDelete.forEach((username) => {
-        fetch(`/api/v1/user/${username}`, {
-            method: "DELETE",
+addUser.addEventListener("click", (e) => {
+    addUserDialog.showModal();
+    const form = addUserDialog.querySelector("#add-user-form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const username = form.querySelector("#username");
+        const password = form.querySelector("#password");
+        const isAdmin = form.querySelector("#isAdmin");
+
+        const user = {
+            username: username.value,
+            password: password.value,
+            admin: isAdmin.checked
+        };
+
+        fetch("/api/v1/user", {
+            method: "POST",
             mode: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
         }).then(async (res) => {
-            const body = await res.json();
-            console.log(body);
+            const data = await res.json();
+            console.log(data);
 
-            // this is bad
             if (res.status >= 400) {
-                unableToRemoveCount++;
-            }
-            if (res.status === 200) {
-                removedCount++;
+                new Notification({ type: "error", text: data.message });
+                return;
             }
 
-            if ((removedCount + unableToRemoveCount) === usersToDelete.length) {
-                if (removedCount !== usersToDelete.length) {
-                    new Notification({ type: "error", text: `${unableToRemoveCount} lietotāju(s) nevarēja noņemt!` });
-                    return;
-                }
-                if (removedCount === 1) {
-                    new Notification({ type: "success", text: `${removedCount} lietotājs noņemts!` });
-                } else {
-                    new Notification({ type: "success", text: `${removedCount} lietotāji noņemti!` });
-                }
-
-            }
+            new Notification({ type: "success", text: data.message });
+            // nav optimāli pārlādēt visu sarakstu, bet pagaidām strādā
+            loadUsers();
         });
-    });
 
-    document.querySelector("#dim-background").style.opacity = 0.8;
-    document.querySelector("#dim-background").classList.toggle("hidden");
-    document.querySelector("#remove-user-prompt").classList.toggle("hidden");
+        username.value = "";
+        password.value = "";
+        isAdmin.checked = false;
+        addUserDialog.close();
+    });
 });
+
+loadUsers();
